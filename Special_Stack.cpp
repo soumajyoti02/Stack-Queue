905881d20878700cf8cd81a86d75e57a4769b1c2
Soumajyoti Sarkar <103195837+soumajyoti02@users.noreply.github.com>
2022-09-12 16:08:37 +0530

    Update Special_Stack.cpp
    SOME RESOURCES: 
    
    1. https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/ [Striver SDE Sheet]
    2. https://practice.geeksforgeeks.org/explore?page=1&category[]=Stack&sortBy=submissions [GFG SDE Sheet]

--- Special_Stack.cpp
+++ Special_Stack.cpp
@@ -1,5 +1,33 @@
+/*
+<<<<<<<<<<<<- PROCEDURE ->>>>>>>>>>>>>>>>>>>>>>
+To find minimum value:
+1. Recursively stored all values of the stack inside a Vector.
+2. Sort the Vector
+3. Return the 0th Index value of that Vector.
+*/
 
 #include<bits/stdc++.h>
+void puting(vector<int>& v, stack<int> s,int key)
+{
+    if(!s.empty())
+    {
+        key = s.top();
+        v.push_back(key);
+        s.pop();
+        puting(v,s,key);
+    }
+    s.push(key);
+}
+
+int getMin(stack<int>& s){
+	// Your code goes here
+	vector<int> v;
+	int key;
+    puting(v,s,key);
+    sort(v.begin(),v.end());
+    return v[0];
+}
+
 void push(stack<int>& s, int a){
 	// Your code goes here
 	s.push(a);
@@ -24,24 +52,4 @@ int pop(stack<int>& s){
 	int t = s.top();
 	s.pop();
 	return t;
-}
-
-void puting(vector<int>& v, stack<int> s,int key)
-{
-    if(!s.empty())
-    {
-        key = s.top();
-        v.push_back(key);
-        s.pop();
-        puting(v,s,key);
-    }
-    s.push(key);
-}
-int getMin(stack<int>& s){
-	// Your code goes here
-	vector<int> v;
-	int key;
-    puting(v,s,key);
-    sort(v.begin(),v.end());
-    return v[0];
 }
